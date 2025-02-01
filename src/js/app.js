2264fe7370abbce854b610172f1f4c45a394fe2e
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-01 22:37:59 +0300

    Fix temperature form input handling and calculation logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,19 @@
 // tempurature-form
 const tempuratureForm = {
-    
+  from: document
+    .querySelector('#tempurature-form')
+    .querySelector('#from-select').value,
+  to: document.querySelector('#tempurature-form').querySelector('#to-select')
+    .value,
+  calculate: function (from, to) {
+    e.preventDefault();
+    console.log(e.target.value);
+  },
   form: document.querySelector('#tempurature-form'),
   fromInput: document
     .querySelector('#tempurature-form')
     .querySelector('#from-value'),
 };
-
-
-
 
 const tempuratureFromInput = tempuratureForm.form.querySelector('#from-value');
 
