0f28bfa5003a53f1290fd6a5ca5775918f414797
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-02 02:19:26 +0300

    Refactor temperature conversion logic to use switch statements for improved readability and maintainability

--- src/js/tempurature.js
+++ src/js/tempurature.js
@@ -8,20 +8,52 @@ const tempuratureForm = {
     const { fromValue, fromSelect, toSelect } = this;
     if (fromValue !== null) {
       let frValue = parseFloat(fromValue);
-      // Farenheight to Celsius
-      if (fromSelect === 'farenheight' && toSelect === 'celsius') {
-        this.toValue = (((frValue - 32) * 5) / 9).toFixed(2);
-      }
-      // Farenheight to Farenheight
-      if (fromSelect === 'farenheight' && toSelect === 'farenheight') {
-        this.toValue = frValue.toFixed(2);
-      }
 
-      if (fromSelect === 'farenheight' && toSelect === 'kelvin') {
-        // (°F + 459.67) × 5/9
-        console.log('Farenheight to Kelvin');
+      switch (fromSelect) {
+        // Farenheight
+        case 'farenheight':
+          switch (toSelect) {
+            case 'farenheight':
+              this.toValue = frValue.toFixed(2);
+              break;
+            case 'celsius':
+              this.toValue = (((frValue - 32) * 5) / 9).toFixed(2);
+              break;
+            case 'kelvin':
+              this.toValue = (((frValue + 459.67) * 5) / 9).toFixed(2);
 
-        this.toValue = (((frValue + 459.67) * 5) / 9).toFixed(2);
+              break;
+          }
+          break;
+
+        // Celsius
+        case 'celsius':
+          switch (toSelect) {
+            case 'farenheight':
+              this.toValue = (frValue * (9 / 5) + 32).toFixed(2);
+              break;
+            case 'celsius':
+              this.toValue = frValue.toFixed(2);
+              break;
+            case 'kelvin':
+              this.toValue = (frValue + 273.15).toFixed(2);
+
+              break;
+          }
+          break;
+        case 'kelvin':
+          switch (toSelect) {
+            case 'farenheight':
+              this.toValue = (frValue * (9 / 5) - 459.67).toFixed(2);
+              break;
+            case 'celsius':
+              this.toValue = (frValue - 273.15).toFixed(2);
+              break;
+            case 'kelvin':
+              this.toValue = frValue.toFixed(2);
+              break;
+          }
+          break;
       }
     }
 
